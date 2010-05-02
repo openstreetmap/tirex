a4e7d2b22aa064708cdc51f19a658cc8a435adc2
Jochen Topf <jochen@remote.org>
2010-05-02 13:07:41 +0000

    tirex utility script to create stats and update tiles regularly

---
+++ utils/tirex-create-stats-and-update-tiles.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+#-----------------------------------------------------------------------------
+#
+#  tirex-create-stats-and-update-tiles.sh
+#
+#  This is an example script that creates statistics for existing tiles and
+#  updates the oldest tiles. You can adapt it to your needs and start it every
+#  few hours from cron.
+#
+#  Note that creating the tile statistics can take a long time, because
+#  every metatile file on the disk has to be found and stat()ed. Depending
+#  on how many tiles you have and hour fast your system is, you might want
+#  to run this script only once per day
+#
+#-----------------------------------------------------------------------------
+
+# how many of the oldest metatiles should be put into the queue?
+OLDESTNUM=5000
+
+#-----------------------------------------------------------------------------
+
+# append output to logfile
+exec >>/var/log/tirex/do-tirex-stats-and-update.log 2>&1
+
+# do not run if this lockfile exists, because the osm database is updated
+# (you only need this if you have some other script that touches this file
+# when the database is beeing updated)
+[ -f /osm/update/osmupdate.lock ] && exit
+
+# directory where the statistics should go
+DIR=/var/lib/tirex/stats
+
+DATE=`date +%FT%H`
+
+echo "--------------------------------------"
+echo -n "Starting "
+date
+
+# stop background rendering
+tirex-rendering-control --debug --stop
+
+# find old statistics files (from earlier runs of this script) and remove them
+find $DIR -type f -mtime +1 -name tiles-\* | xargs --no-run-if-empty rm
+
+# check tile directory and create statistics
+tirex-tiledir-check --list=$DIR/tiles-$DATE.csv --stats=$DIR/tiles-$DATE.stats
+
+# link tiles.stats to newest statistics file
+rm -f $DIR/tiles.stats
+ln -s $DIR/tiles-$DATE.stats $DIR/tiles.stats
+
+# find $OLDESTNUM oldest metatiles...
+sort --field-separator=, --numeric-sort --reverse $DIR/tiles-$DATE.csv | head -$OLDESTNUM | cut -d, -f4 >$DIR/tiles-$DATE.oldest
+
+# ...and add them to tirex queue
+tirex-batch --prio=20 <$DIR/tiles-$DATE.oldest
+
+# re-start background rendering
+tirex-rendering-control --debug --continue
+
+echo -n "Done "
+date
+
+#-- THE END ----------------------------------------------------------------------------
