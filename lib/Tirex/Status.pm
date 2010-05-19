65c2ded68c51241935d8f86f0f233baff8786e13
Teemu Koskinen <teemu.koskinen@saunalahti.fi>
2010-05-19 08:25:07 +0000

    Fix my previous commit, should still use IPC::SysV::IPC_RMID

--- lib/Tirex/Status.pm
+++ lib/Tirex/Status.pm
@@ -60,9 +60,9 @@ sub new
     if ($self->{'master'})
     {
         my $id = shmget($SHMKEY, 0, 0);
-        shmctl($id, IPC_RMID, 0) if (defined($id));
+        shmctl($id, IPC::SysV::IPC_RMID, 0) if (defined($id));
         $id = semget($SHMKEY, 0, 0);
-        semctl($id, IPC_RMID, 0, 0) if (defined($id));
+        semctl($id, IPC::SysV::IPC_RMID, 0, 0) if (defined($id));
     }
 
     $self->{'share'} = IPC::ShareLite->new(
