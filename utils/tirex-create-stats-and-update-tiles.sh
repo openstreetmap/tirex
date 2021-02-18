afa10602ee541c58a5fa030af6cf613095d7798d
Stephan <stephankn@users.noreply.github.com>
2021-02-18 17:41:14 +0100

    if configured, use stats_dir from config file
    otherwise fall back to hard-coded /var/lib/tirex/stats
    
    This allows #17

--- utils/tirex-create-stats-and-update-tiles.sh
+++ utils/tirex-create-stats-and-update-tiles.sh
@@ -67,7 +67,10 @@ exec >>/var/log/tirex/tirex-create-stats-and-update-tiles.log 2>&1
 [ -f /osm/update/osmupdate.lock ] && exit
 
 # directory where the statistics should go
-DIR=/var/lib/tirex/stats
+DIR=$(sed -n 's/^stats_dir=//p' /etc/tirex/tirex.conf)
+if [ -z "$DIR" ]; then
+  DIR=/var/lib/tirex/stats
+fi
 
 DATE=`date +%FT%H`
 
